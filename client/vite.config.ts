c619e809814eb40bc6239f4bb6f0f527130b8651
Sahil kumar <supersen14@gmail.com>
2025-11-06 05:21:07 +0530

    deploy to cloudflare

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    [cloudflare({ viteEnvironment: { name: 'ssr' } })],
     tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
